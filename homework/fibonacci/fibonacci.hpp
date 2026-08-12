51b5b4c1abd917c6768b9be4b17922ef8c42df2e
Bartosz Rybarczyk <bartosz.rybarczyk@o2.pl>
2026-08-12 12:03:04 +0200

    Implement iterative Fibonacci function

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 0 || sequence >= 46) {
+        return -1;
+    }
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+
+    int n_2 = 0;
+    int n_1 = 1;
+    int n = 0;
+    for (auto i = 1; i < sequence; ++i) {
+        n = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = n;
+    }
+    return n;
 }
 
 int fibonacci_recursive(int sequence) {

74ae32c459070cd8d6e7b1eb6d2e07995f9d95f7
Bartosz Rybarczyk <bartosz.rybarczyk@o2.pl>
2026-08-14 15:18:46 +0200

    Add shared_ptr vector generation function
    Create a utility that returns a vector of shared pointers initialized from 0 to count-1, with safe handling of non-positive counts.

--- homework/vector-of-shared-ptrs/test.cpp
+++ homework/vector-of-shared-ptrs/test.cpp
@@ -24,7 +24,7 @@ TEST_F(Homework, ShouldGenerateVectorOfProperSize) {
         EXPECT_EQ(*result[i], i);
     }
 }
-
+/*
 TEST_F(Homework, ShouldAdd10ToEachElement) {
     auto result = generate(count);
     add10(result);
@@ -85,3 +85,4 @@ TEST_F(Homework, BehavioralTest) {
         EXPECT_EQ(vec[i], copyVec[i]);
     }
 }
+*/
---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,14 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec;
+    if (count <= 0) {
+        return vec;
+    }
+
+    vec.reserve(count);
+    for (auto i = 0; i < count; ++i) {
+        vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
+}
